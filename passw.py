27e791f5b7d768039deea66439272acf71789724
Emir Can Tanyıldız <117983065+EmircanTanyildiz@users.noreply.github.com>
2024-02-17 21:37:30 +0300

    Create passw.py

---
+++ passw.py
@@ -0,0 +1,6 @@
+class myInfo:
+    def __init__(self):
+        u = input("Kullanici Adinizi Giriniz: ")
+        p = input("Sifrenizi Giriniz: ")
+        self.username = u
+        self.password = p
